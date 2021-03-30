6f1a245222ddad97e2eae433aa224a4f353fa21e
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-03-30 20:58:56 -0300

    Iniciando Projeto
    iniciando projeto appDeveloper - Escalapp

---
+++ app/src/main/java/java/project/escalapp/splashScreen.java
@@ -0,0 +1,28 @@
+package java.project.escalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
