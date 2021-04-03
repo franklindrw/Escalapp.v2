07e762f2e23374332f7e4f3ebd0717dec657dc70
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-04-03 01:37:12 -0300

    Inserindo as Telas
    Atualizando o projeto com a inclusão das telas e da mecanica para trocar usando a nav bar.

--- app/src/main/java/java/project/escalapp/MainActivity.java
+++ app/src/main/java/java/project/escalapp/MainActivity.java
@@ -1,8 +1,13 @@
 package java.project.escalapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //iniciando o menu para trocar entre as telas
+        BottomNavigationView bottomNav = findViewById(R.id.menus_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new homeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+
+                        case R.id.homeFragment:
+                            selectedFragment = new homeFragment();
+                            break;
+
+                        case R.id.folgasFragment:
+                            selectedFragment = new folgasFragment();
+                            break;
+
+                        case R.id.atividadesFragment:
+                            selectedFragment = new atividadesFragment();
+                            break;
+
+                        case R.id.maisFragment:
+                            selectedFragment = new atividadesFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
