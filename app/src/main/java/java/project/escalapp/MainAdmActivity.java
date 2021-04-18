6470d1ffdef27087d5103102d87d775b4b0d055c
Franklin Almeida Campos <81038899+franklindrw@users.noreply.github.com>
2021-04-17 23:22:39 -0300

    Main administrador
    Adicionando a main do administrador

---
+++ app/src/main/java/java/project/escalapp/MainAdmActivity.java
@@ -0,0 +1,62 @@
+package java.project.escalapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainAdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_adm);
+
+
+        //Ocultar a Action Bar
+
+        //iniciando o menu para trocar entre as telas
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new homeFragment()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+
+                        case R.id.homeFragment:
+                            selectedFragment = new homeFragment();
+                            break;
+
+                        case R.id.folgasFragment:
+                            selectedFragment = new folgasFragment();
+                            break;
+
+                        case R.id.atividadesFragment:
+                            selectedFragment = new atividadesFragment();
+                            break;
+
+                        case R.id.menuAdmFragment:
+                            selectedFragment = new menuAdmFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
